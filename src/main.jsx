22434722f1b971e4910b5298c9642492333b7481
qianhui <zhengqianhui@sjtu.edu.cn>
2025-06-19 21:51:04 -0400

    Deploy to custom domain

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
 import SIXRouter from './components/nav/SIXRouter.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <SIXRouter />
