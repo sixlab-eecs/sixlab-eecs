ac9ff411e0ac1d364c1504dcb8862c9277d33fb9
yimjisu <60869086+yimjisu@users.noreply.github.com>
2025-12-06 21:28:11 -0500

    [update] change font, banner

---
+++ src/data/new.js
@@ -0,0 +1,25 @@
+
+const news = [
+    {
+        time: "[07/2025]",
+        description: "Three new papers conditionally accepted at ACM UIST 2025, see you in Busan, Korea!",
+        link: "",
+    },
+    {
+        time: "[04/2025]",
+        description: "UMich ECE welcomes Prof. Junyi Zhu, PI of the SIX Lab, in a featured Q&A article.",
+        link: "https://ece.engin.umich.edu/stories/qa-with-new-faculty-member-junyi-zhu"
+    },
+    {
+        time: "[01/2025]",
+        description: "We are actively recruiting undergraduate and graduate researchers (PhD & Postdoc)!",
+        link: "/join"
+    },
+    {
+        time: "[01/2025]",
+        description: "The Sensing Intelligence and eXperience Lab at UMich is established!",
+        link: "",
+    },
+]
+
+export default news;
